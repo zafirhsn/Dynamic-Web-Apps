686797a767576ddd1a86ea36a23aa9926ae9d113
Zafir Hasan <zafirhssn@gmail.com>
2018-10-14 01:24:35 -0400

    Fixing problems with Oauth

--- memebot/bot.js
+++ memebot/bot.js
@@ -1,8 +1,44 @@
 // require twitter bot and reddit bot packages
 var Twit = require('twit');
+var Red = require('reddit-oauth');
 var config = require('./configure');
-console.log(config);
-console.log(Twit);
+
+var twitter = new Twit(config.twitAuth);
+var reddit = new Red(config.redditAuth);
+
+
+function errHandle(err, data, response) {
+    if (err) {
+        console.log("Did not post");
+        console.log(err);
+    }
+    else {
+        console.log("Posted");
+    }
+}
+
+reddit.passAuth(
+    'FreshestMemeBot',
+    'nP88kHGzNj25',
+    function(success) {
+        if (success) {
+            console.log(reddit.access_token);
+        }
+        else {
+            console.log("did not work");
+        }
+    }
+);
+
+// reddit.api.get('api/v1/me', {}, function(err, responseCode, response) {
+//     // console.log(responseCode);
+//     // console.log(response);
+// });
+
+// Twitter posting is working
+// twitter.post('statuses/update', {status: 'This is a test for memebot. Post using twit module from NPM'}, function(err, data, response) {
+//     errHandle(err, data, response);
+// });
 
 
 
--- memebot/configure.js
+++ memebot/configure.js
@@ -1,12 +1,19 @@
-module.exports = {
-    
-    
-    username: '',
-    password: '',
-    app_id: '', 
-    api_secret: '',
-    user_agent: '',
+module.exports.redditAuth = {
+        // username: 'FreshestMemeBot',
+        // password: 'nP88kHGzNj25',
+        app_id: 'lTg0SwQHHcetIA', 
+        app_secret: 'Hmw8tv7AsqAIEvCdOp9xwAE70v8',
+        //user_agent: '', 
+        
+        // automatic_retries: true,
+        // api_requests_per_minute: 60 // api requests will be spread out in order to play nicely with Reddit
+}
 
-
-    
+module.exports.twitAuth = {
+        consumer_key:         'GMfkCYy1pWAtqr1w25UQZAY6e',
+        consumer_secret:      'kwj1ej6S8ie88WVGq877hchkiipdSc8v6eqPnXSsJ4ThyRdd5y',
+        access_token:         '1047161814713360386-KLkSREXcVdhSXEZCBw0Cv3pWNwk7br',
+        access_token_secret:  'huD28PW3Rk3Lz8tHpFXXfM2Nx3NTmj7T1do9LsTexF8H0',
+        timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+        strictSSL:            true,     // optional - requires SSL certificates to be valid.
 }
