5dbabe3427d96a92be09844ab35d3f8c8dbe6762
Zafir Hasan <zafirhsn@gmail.com>
2018-10-15 19:13:29 -0400

    Almost done

--- memebot/bot.js
+++ memebot/bot.js
@@ -6,7 +6,8 @@ const config = require('./configure');
 const twitter = new Twit(config.twitAuth);
 const reddit = new Red(config.redditAuth);
 
-const subList = ['memes', 'BikiniBottomTwitter', 'dankmemes', 'wholesomememes', 'MemeEconomy', 'BlackPeopleTwitter', 'WhitePeopleTwitter', 'me_irl', 'meirl']; 
+const subList = ['memes', 'BikiniBottomTwitter', 'dankmemes', 'wholesomememes', 'MemeEconomy', 'BlackPeopleTwitter', 'WhitePeopleTwitter', 'me_irl', 'meirl'];
+let topMemes = [];
 
 function errHandle(err, data, response) {
     if (err) {
@@ -18,14 +19,27 @@ function errHandle(err, data, response) {
     }
 }
 
-reddit.ratelimitRemaining;
+// console.log(reddit.ratelimitRemaining);
 
-reddit.getSubmission('9o0sma').expandReplies({limit: Infinity, depth: Infinity}).then(console.log);
+
+// for (let i = 0; i < subList.length; i++) {
+    reddit.getSubreddit(subList[0]).getTop({time: 'day'}).then(data => { topMemes[0] = { score: data[0].score, url: data[0].url, nsfw: data[0].over_18 }, console.log(topMemes[0])});
+// }
+
+
+
+// reddit.getSubreddit('memes').getTop({time: "day"}).length.then(
+//     function(data) {
+//         topMemes[0] = data;
+//         console.log(topMemes[0]);
+//     }
+// );
+
 
 
 // reddit.getSubreddit('memes').
 
-twitter.stream('' )
+//twitter.stream('' )
 
 // Twitter posting is working
 // twitter.post('statuses/update', {status: 'This is a test for memebot. Post using twit module from NPM'}, function(err, data, response) {
