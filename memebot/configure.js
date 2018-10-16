454734251c1e3181b1e380bf41accaf85c9f8e41
Zafir Hasan <zafirhsn@gmail.com>
2018-10-16 11:55:13 -0400

    Adding README.md

---
+++ memebot/configure.js
@@ -0,0 +1,20 @@
+module.exports.redditAuth = {
+
+        user_agent: 'Node.js:Freshest Meme Bot:v1.0 (by /u/FreshesMemeBot)',
+        clientId: '6fFGX0sYgdW4Gg', 
+        clientSecret: 'rBLbCcC-5-CSAY10ZrWbyRVm_Ao',
+        username: 'FreshestMemeBot',
+        password: 'nP88kHGzNj25',
+        // redirect_uri: 'http://localhost:8080',  
+        // automatic_retries: true,
+        // api_requests_per_minute: 60 // api requests will be spread out in order to play nicely with Reddit
+}
+
+module.exports.twitAuth = {
+        consumer_key:         'GMfkCYy1pWAtqr1w25UQZAY6e',
+        consumer_secret:      'kwj1ej6S8ie88WVGq877hchkiipdSc8v6eqPnXSsJ4ThyRdd5y',
+        access_token:         '1047161814713360386-KLkSREXcVdhSXEZCBw0Cv3pWNwk7br',
+        access_token_secret:  'huD28PW3Rk3Lz8tHpFXXfM2Nx3NTmj7T1do9LsTexF8H0',
+        timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+        strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
