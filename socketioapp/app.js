9cb6776b066f330771905fcacb4f81d8989bdf9b
Zafir Hasan <zafirhsn@gmail.com>
2018-11-14 14:16:48 -0500

    Finished socket.io tutorial

---
+++ socketioapp/app.js
@@ -0,0 +1,25 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+// const http = require('http').Server(app);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (data) => {
+    console.log('A user connected');
+    data.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+    data.emit('news', {hello: 'world'});
+    
+});
+
+// app.listen(3000, () => {
+//     console.log('Server is listening on port 3000');
+// });
+
+http.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
